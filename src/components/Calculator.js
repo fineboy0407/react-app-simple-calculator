52da9525cbb9d81a638a5137cc409f4e800b9d55
fineboy0407 <levilee.fine@gmail.com>
2021-03-19 18:23:43 +0530

    React Hooks are added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,85 +6,61 @@ import DisplayScreen from './DisplayScreen'
 import ShowButtons from './ShowButtons'
 import ErrorComponent from './ErrorComponent'
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
 
-    this.state = {
-      displayExpression: '',
-      displayValue: 0,
-      showError: false
-    }
+  // initial state
+  const calcExpression = {
+    displayExpression: '',
+    displayValue: 0,
+    showError: false
   }
 
+  // set the initial state along with its modifier function
+  const [calcExp, setCalcExp] = useState(calcExpression)
 
-  computeValue() {
-
-    this.setState({
-      displayValue: evaluate(this.state.displayExpression)
-    })
-
-    this.state.displayExpression = ''
+  // the actual computation using Math.js
+  const computeValue = () => {
+    setCalcExp({ ...calcExp, displayValue: evaluate(calcExp.displayExpression), displayExpression: '' })
   }
 
-  computeDisplayExpression(event) {
-    event.persist()
+  const computeDisplayExpression = (e) => {
+    if (e.target.id == 'CL') {
+      setCalcExp({ ...calcExp, displayExpression: '', displayValue: 0 })
 
-    if (event.target.id == 'CL') {
-      this.setState({
-        displayExpression: '',
-        displayValue: 0
-      })
+    } else if (calcExp.displayExpression == '' && e.target.id.match(/[0-9]/g) == null) {
 
-    } else if (this.state.displayExpression == '' && event.target.id.match(/[0-9]/g) == null) {
-
-      // if a symbol comes without any number
-      this.setState({
-        displayExpression: ''
-      })
-
-      // show the error notification
-      this.setState((prevState, props) => ({
-        showError: true
-      }))
+      // if a symbol comes without any number show the error notification
+      setCalcExp({ ...calcExp, displayExpression: '', showError: true })
 
       // after 3ms hide the notification
       setTimeout(() => {
-        this.setState((prevState, props) => ({
-          showError: false
-        }))
+        setCalcExp({ ...calcExp, showError: false })
       }, 3000)
 
     } else {
 
-      this.setState((prevState, props) => ({
-        displayExpression: prevState.displayExpression + event.target.id
-      }))
+      setCalcExp({
+        ...calcExp,
+        displayExpression: calcExp.displayExpression + e.target.id
+      })
     }
   }
 
-  render() {
-    let { displayValue, displayExpression } = this.state
+  const { displayValue, displayExpression, showError } = calcExp
 
-    var err = ''
-    if (this.state.showError == true) {
-      err = <ErrorComponent msg={'Please enter a number first'} />
-    }
+  return (
+    <div>
+      {showError && <ErrorComponent msg={'Please enter a number first'} />}
 
-    return (
-      <div>
-        {err}
+      <DisplayScreen
+        displayExpression={displayExpression}
+        displayValue={displayValue} />
 
-        <DisplayScreen
-          displayExpression={displayExpression}
-          displayValue={displayValue} />
-
-        <ShowButtons
-          computeValue={this.computeValue.bind(this)}
-          computeDisplayExpression={this.computeDisplayExpression.bind(this)} />
-      </div>
-    );
-  }
+      <ShowButtons
+        computeValue={computeValue}
+        computeDisplayExpression={computeDisplayExpression} />
+    </div>
+  );
 }
 
 export default Calculator;
