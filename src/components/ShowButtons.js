ecebbf5047f6c2e878776a686d61a79795ae336d
fineboy0407 <levilee.fine@gmail.com>
2021-12-28 16:30:34 +0530

    prettier applied

--- src/components/ShowButtons.js
+++ src/components/ShowButtons.js
@@ -1,24 +1,44 @@
 // ShowButtons component
 export default ({ computeValue, computeDisplayExpression }) => {
-
 	const CALCULATOR_BUTTONS = [
-		'(', ')', '^', 'CL',
-		'7', '8', '9', '/',
-		'4', '5', '6', '*',
-		'1', '2', '3', '-',
-		'.', '0', '=', '+'
+		"(",
+		")",
+		"^",
+		"CL",
+		"7",
+		"8",
+		"9",
+		"/",
+		"4",
+		"5",
+		"6",
+		"*",
+		"1",
+		"2",
+		"3",
+		"-",
+		".",
+		"0",
+		"=",
+		"+",
 	]
 	return (
 		<div className="show-buttons">
-
-			{
-				CALCULATOR_BUTTONS.map((ele, index) => {
-					return (
-						<a key={'calc-' + ele} href="#" id={ele} className="btn"
-							onClick={(ele != '=') ? computeDisplayExpression : computeValue} >{ele}</a>
-					)
-				})
-			}
+			{CALCULATOR_BUTTONS.map((ele, index) => {
+				return (
+					<a
+						key={"calc-" + ele}
+						href="#"
+						id={ele}
+						className="btn"
+						onClick={
+							ele != "=" ? computeDisplayExpression : computeValue
+						}
+					>
+						{ele}
+					</a>
+				)
+			})}
 		</div>
-	);
-};
+	)
+}
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,9 @@
 export default () => {
   return (
     <div className="title">
-      <span className="caption">Simple <strong>Calculator</strong>
+      <span className="caption">
+        Simple <strong>Calculator</strong>
       </span>
     </div>
-  );
-};
+  )
+}
