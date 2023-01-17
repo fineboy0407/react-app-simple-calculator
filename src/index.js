bee0b135f7617389be228892d8976885882de90a
fineboy0407 <levilee.fine@gmail.com>
2023-01-17 11:52:34 +0530

    React 18 fix added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import the render
-import { render } from "react-dom"
+import { createRoot } from "react-dom/client";
 
 // the actual App component
-import App from "./App"
+import App from "./App";
 
-render(<App />, document.getElementById("app"))
+createRoot(document.getElementById("app")).render(<App />);
